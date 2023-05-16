5fca072b06d9379ab655c7fcbe5d83d1d464d3e4
Omar Ahmed <omarahmedabdelhamid@outlook.com>
2023-05-16 06:46:10 +0300

    first commit

---
+++ Untitled4.m
@@ -0,0 +1,145 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%         part 2       %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Step 1: Load and convert the image to grayscale
+clc
+close all
+
+reduction_factor =5;
+scan_range=2;
+image = imread('coins.jpg');
+img_bw = rgb2gray(image);
+
+binary_image = zeros(size(img_bw));
+% turn to black and white
+
+binary_image(img_bw>78) = 255;
+
+figure;imshow(binary_image)
+[height, width] = size(binary_image);
+label_counter=0;
+current_label =0;
+label = containers.Map;
+
+objectx =[];
+objecty =[];
+
+boundryx =[];
+boundryy =[];
+
+% label('1,7')=1;
+% label('2,8')=2;
+%
+% isKey(label,('1,7'))
+% label('1,7')
+mod_binary_reduction_image = ones(size(binary_image));
+mod_binary_reduction_image = mod_binary_reduction_image*255;
+
+% for i = 1:reduction_factor
+%     for j = 1:reduction_factor
+%         mod_binary_reduction_image(i,:)=0;
+%         mod_binary_reduction_image(height-i,:)=0;
+%         mod_binary_reduction_image(:,width-j)=0;
+%         mod_binary_reduction_image(:,j)=0;
+%         
+%     end
+% end
+
+constant_i =0;
+constant_j =0;
+figure;imshow(mod_binary_reduction_image)
+for i = 0+1:height
+    for j = 0+1:width
+        if binary_image(i,j)==0
+            
+            while(i-reduction_factor+constant_i)<1
+                constant_i=constant_i+1;
+            end
+            while(j-reduction_factor+constant_j)<1
+                constant_j=constant_j+1;
+            end
+            mod_binary_reduction_image(i-reduction_factor+constant_i:i+reduction_factor,j-reduction_factor+constant_j:j+reduction_factor)=0;
+%              mod_binary_image(i,j)=255;
+            constant_i =0;
+            constant_j =0;
+
+        end
+    end
+end
+figure;imshow(mod_binary_reduction_image)
+% smoothed = conv(mod_binary_reduction_image((1:end-1),:),mod_binary_reduction_image((2:end),:),'same');
+% figure;imshow(smoothed)
+
+mod_binary_image = zeros(size(binary_image));
+for i = 1:height
+    for j = 1:width
+        if mod_binary_reduction_image(i,j)==255
+            
+            mod_binary_image(i-reduction_factor:i+reduction_factor,j-reduction_factor:j+reduction_factor)=255;
+% mod_binary_image(i-reduction_factor:i+reduction_factor,j)=255;
+%              mod_binary_image(i,j)=255;
+        end
+    end
+end
+
+figure;imshow(mod_binary_image)
+binary_image = mod_binary_image;
+% binary_image = mod_binary_reduction_image;
+
+for i = 1:height
+    for j = 1:width
+        
+        if binary_image(i,j) == 0
+            continue
+        end
+        if binary_image(i,j)==255
+            current_label =0;
+            for i_nieghbour = -scan_range:scan_range
+                if current_label
+                    break
+                end
+                for j_nieghbour = -scan_range:scan_range
+                    if isKey(label,strcat(num2str(i+i_nieghbour),',',num2str(j+j_nieghbour)))
+                        current_label = label(strcat(num2str(i+i_nieghbour),',',num2str(j+j_nieghbour)));
+                        label(strcat(num2str(i),',',num2str(j))) = current_label;
+                        objectx(current_label, end+1) =  i;
+                        objecty(current_label, end+1) = j;
+                        break;
+                        
+                    end
+                end
+            end
+            if ~current_label
+                label_counter = label_counter+1 ;
+                label(strcat(num2str(i),',',num2str(j))) = label_counter;
+                objectx(end+1,1)= [i];
+                objecty(end+1,1) =  [j];
+                
+            end
+        end
+        
+    end
+end
+% [object_number, elemntsX] = size(objectx);
+% [elemntsY, object_number] = size(objecty);
+
+
+% image_mod = zeros(size(image));
+for i = 1: label_counter
+
+    current_x=find(objectx(i,:));
+    current_y=find(objecty(i,:));
+    centerx = round(mean(objectx(i,current_x(:))))
+    centery = round(mean(objecty(i,current_y(:))))
+        image(centerx-3:centerx+3,centery-3:centery+3,:)=250;
+
+    
+    for pos = 1: length(current_x)
+        
+%         image(objectx(i,current_x(pos)),objecty(i,current_y(pos)),1)=250;
+%         image(objectx(i,current_x(pos)),objecty(i,current_y(pos)),2)=250;
+%         image(objectx(i,current_x(pos)),objecty(i,current_y(pos)),3)=250;
+    end
+
+end
+% image()=(255,255,255)
+figure;imshow(image)
+fprintf("done\n")
---
+++ first_part.m
@@ -0,0 +1,58 @@
+% Read the input image
+image = imread('Ocean.bmp');
+
+% Define the parameters
+A1 = 30;
+B1 = 20;
+C1 = 180;
+D1 = 230;
+
+A2 = 70;
+B2 = 20;
+C2 = 140;
+D2 = 240;
+image_path = 'Ocean.bmp';
+
+
+% Transformation parameters for Ocean_1.bmp
+
+transformed_image1=contrast_fn(image_path, A1, B1, C1, D1);
+
+% Transformation parameters for Ocean_2.bmp
+
+transformed_image2=contrast_fn(image_path, A2, B2, C2, D2);
+transformed_image3=contrast_fn(image_path, 20, 20, 200, 200); %linear should return original 
+transformed_image4=contrast_fn(image_path, 125, 0, 125, 255); %binary image
+imwrite(transformed_image1, 'Ocean_1.bmp');
+imwrite(transformed_image2, 'Ocean_2.bmp');
+imwrite(transformed_image3, 'Ocean_3.bmp');
+imwrite(transformed_image4, 'Ocean_4.bmp');
+
+
+
+%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%         functions       %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%     
+function transformed_image=contrast_fn(image_path, r1, s1, r2, s2)
+    % Load the grayscale image
+    img = imread(image_path);
+
+    % Normalize image intensity values
+    img = im2double(img);
+
+    % Contrast stretching transformation
+    out = zeros(size(img));
+
+    out(img < r1) = s1 / r1 * img(img < r1);
+    out(img >= r1 & img <= r2) = ((s2 - s1) / (r2 - r1)) * (img(img >= r1 & img <= r2) - r1) + s1;
+    out(img > r2) = ((1 - s2) / (1 - r2)) * (img(img > r2) - r2) + s2;
+
+    % Convert back to original intensity range
+    transformed_image = im2uint8(out);
+
+    % Save the transformed image
+    [~, name, ~] = fileparts(image_path);
+    transformed_path = [name '_transformed.bmp'];
+    imwrite(transformed_image, transformed_path);
+
+    fprintf('Transformed image saved as: %s\n', transformed_path);
+end
